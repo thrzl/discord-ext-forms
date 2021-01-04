60e9866876c52fefb5ad8945da2ec0b86819ccee
Mikey <terry.bazemoreiii@gmail.com>
2021-01-04 09:20:41 -0500

    fixed conf.py a bit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, Mikey B'
 author = 'Mikey B'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.5.1'
 
 
 # -- General configuration ---------------------------------------------------
