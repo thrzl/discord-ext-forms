c753e6842d29da62c8cb627f674b2b37bbaa0b1d
Mikey <terry.bazemoreiii@gmail.com>
2021-01-06 12:43:12 -0500

    docs please just work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,11 +31,11 @@ release = '2.5.3b'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon','sphinx.ext.autosummary','sphinx.ext.viewcode','numpydoc']
+extensions = ['autoapi.extension','sphinx.ext.napoleon','sphinx.ext.viewcode','numpydoc']#,'sphinx.ext.autodoc','sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
+autoapi_dirs = ['../../discord']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
