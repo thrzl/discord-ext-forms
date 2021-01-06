7963da9cb8f5240683cd448091a1dfae9e9abaf6
Mikey <terry.bazemoreiii@gmail.com>
2021-01-06 14:00:14 -0500

    try to update the code again

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
         name='discord-ext-forms',
-        version='2.3.5b',
+        version='2.3.beta-6',
         packages=["discord.ext.forms"],
         author='Mikey B',
         author_email='mikey@mikeyo.ml',
