23c408400bee4df48077b36f886ca3b2d1457ad6
Mikey <terry.bazemoreiii@gmail.com>
2021-01-06 11:51:47 -0500

    did buncha stuff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Mikey B'
 author = 'Mikey B'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.4'
+release = '2.5.3b'
 
 
 # -- General configuration ---------------------------------------------------
