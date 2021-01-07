2ef3fb5b5d0bb7629a578c1ab26c1d140a1dae7f
Mikey <terry.bazemoreiii@gmail.com>
2021-01-07 15:07:59 -0500

    github and pypi are being smallbrain rn

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
         name='discord-ext-forms',
-        version='2.6.10',
+        version='2.6.11',
         packages=["discord.ext.forms"],
         author='Mikey B',
         author_email='mikey@mikeyo.ml',
