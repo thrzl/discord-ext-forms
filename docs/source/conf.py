aa584e4fa79e73e13c4425a0edad8bdccd67a4b7
Mikey <terry.bazemoreiii@gmail.com>
2021-02-13 22:01:06 -0500

    🔧 added apidoc to sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ release = '2.6.12'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon','sphinx.ext.viewcode','numpydoc']#,'sphinx.ext.autodoc','sphinx.ext.autosummary']
+extensions = ['sphinx.ext.napoleon','sphinx.ext.viewcode','numpydoc','sphinx.ext.apidoc']#,'sphinx.ext.autodoc','sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
