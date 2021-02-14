9791b90321d0c4f894c702efedbed678e3988479
Mikey <terry.bazemoreiii@gmail.com>
2021-02-13 22:47:02 -0500

    also E

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,10 +33,10 @@ release = "2.6.12"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "autoapi.extensions",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "numpydoc",
+    "sphinx.ext.apidoc",
 ]  # ,'sphinx.ext.autodoc','sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
