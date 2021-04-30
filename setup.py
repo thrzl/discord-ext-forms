925b0543de1a60a2161aa9d8659ab075a2d088bc
Mikey <mikey@mikeyo.ml>
2021-04-30 18:57:56 -0400

    🔖 bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
         name='discord-ext-forms',
-        version='2.8.0',
+        version='2.8.1',
         packages=["discord.ext.forms"],
         author='Mikey B',
         author_email='mikey@mikeyo.ml',
